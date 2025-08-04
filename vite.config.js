5e1a01ec229a2d5b7e3159869d5c1232a41b2090
AchroDev <achrodev@proton.me>
2025-08-03 19:15:57 -0500

    CHANGELOG 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"
+import path from "path";
+import postcss from "./postcss.config.js"; // Import the postcss config
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-   resolve: { // 2. Add this resolve block
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  css: { // Explicitly define the postcss config
+    postcss,
+  },
+  // Vite options tailored for Tauri development
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
@@ -29,7 +30,6 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
